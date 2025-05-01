ea665f16390bb10fc7d4377a793bccfb08004740
Gurbinder <gurbindersingh8727@gmail.com>
2025-05-02 01:57:57 +0530

    refactor(web): reset apps/web

--- apps/web/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  transpilePackages: ["@invoicely/ui", "@invoicely/utilities"],
-};
-
-export default nextConfig;
