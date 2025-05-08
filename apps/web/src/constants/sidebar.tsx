a22b4366d9a7dfb463817d5b6a28463e95d540cc
Gurbinder <gurbindersingh8727@gmail.com>
2025-05-08 13:17:46 +0530

    refactor(web): hide sidebar options before releasing

--- apps/web/src/constants/sidebar.tsx
+++ apps/web/src/constants/sidebar.tsx
@@ -1,25 +1,25 @@
-import { ReceiptIcon, SquareWandSparkleIcon, GaugeIcon } from "@/assets/icons";
+import { ReceiptIcon } from "@/assets/icons";
 import type { ISidebar } from "@/types";
 import { LINKS } from "./links";
 
 export const SIDEBAR_ITEMS: ISidebar = {
-  Navigation: [
-    {
-      name: "Dashboard",
-      url: LINKS.DASHBOARD,
-      icon: <SquareWandSparkleIcon />,
-    },
-    {
-      name: "Invoices",
-      url: LINKS.INVOICES,
-      icon: <ReceiptIcon />,
-    },
-    {
-      name: "Analytics",
-      url: LINKS.ANALYTICS,
-      icon: <GaugeIcon />,
-    },
-  ],
+  // Navigation: [
+  //   {
+  //     name: "Dashboard",
+  //     url: LINKS.DASHBOARD,
+  //     icon: <SquareWandSparkleIcon />,
+  //   },
+  //   {
+  //     name: "Invoices",
+  //     url: LINKS.INVOICES,
+  //     icon: <ReceiptIcon />,
+  //   },
+  //   {
+  //     name: "Analytics",
+  //     url: LINKS.ANALYTICS,
+  //     icon: <GaugeIcon />,
+  //   },
+  // ],
   Create: [
     {
       name: "Create Invoice",
