09456fd15dbad6af01ae97cefe550259379ae6af
Gurbinder <gurbindersingh8727@gmail.com>
2025-05-09 21:26:02 +0530

    fix(web): code field css

--- apps/web/src/components/layout/marketing/blogs/components/code.tsx
+++ apps/web/src/components/layout/marketing/blogs/components/code.tsx
@@ -1,8 +1,16 @@
 import { Pre, CodeBlock } from "fumadocs-ui/components/codeblock";
+import { cn } from "@/lib/utils";
 
 export const Code = ({ ...props }) => {
   return (
-    <CodeBlock {...props}>
+    <CodeBlock
+      {...props}
+      className={cn(
+        props.className,
+        "-mx-12 my-12 rounded-none border-x-0 border-b border-dashed !bg-transparent",
+        "[&>div]:first-of-type:!text-primary [&>div]:first-of-type:border-b [&>div]:first-of-type:border-dashed [&>div]:first-of-type:bg-transparent",
+      )}
+    >
       <Pre>{props.children}</Pre>
     </CodeBlock>
   );
