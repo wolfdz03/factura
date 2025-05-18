164d2afae1816f2c285235f150102e5a70f27a7c
Gurbinder <gurbindersingh8727@gmail.com>
2025-05-18 13:28:52 +0530

    feat(web): advance data-table with filters

--- apps/web/src/constants/sidebar.tsx
+++ apps/web/src/constants/sidebar.tsx
@@ -3,23 +3,23 @@ import type { ISidebar } from "@/types";
 import { LINKS } from "./links";
 
 export const SIDEBAR_ITEMS: ISidebar = {
-  // Navigation: [
-  //   {
-  //     name: "Dashboard",
-  //     url: LINKS.DASHBOARD,
-  //     icon: <SquareWandSparkleIcon />,
-  //   },
-  //   {
-  //     name: "Invoices",
-  //     url: LINKS.INVOICES,
-  //     icon: <ReceiptIcon />,
-  //   },
-  //   {
-  //     name: "Analytics",
-  //     url: LINKS.ANALYTICS,
-  //     icon: <GaugeIcon />,
-  //   },
-  // ],
+  Navigation: [
+    // {
+    //   name: "Dashboard",
+    //   url: LINKS.DASHBOARD,
+    //   icon: <SquareWandSparkleIcon />,
+    // },
+    {
+      name: "Invoices",
+      url: LINKS.INVOICES,
+      icon: <ReceiptIcon />,
+    },
+    // {
+    //   name: "Analytics",
+    //   url: LINKS.ANALYTICS,
+    //   icon: <GaugeIcon />,
+    // },
+  ],
   Create: [
     {
       name: "Create Invoice",
