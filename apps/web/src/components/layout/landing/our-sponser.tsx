6737e32cd823c03b5b539b2707726db795f2da8f
Gurbinder <gurbindersingh8727@gmail.com>
2025-07-05 23:38:32 +0530

    feat: update css issues

--- apps/web/src/components/layout/landing/our-sponser.tsx
+++ apps/web/src/components/layout/landing/our-sponser.tsx
@@ -52,9 +52,7 @@ const OurSponser = () => {
         {sponsers.map((sponser, index) => (
           <div
             key={sponser.name}
-            className={cn(
-              "grid grid-flow-row grid-cols-1 border-b border-dashed last:border-b-0 sm:h-[150px] sm:grid-cols-3",
-            )}
+            className={cn("grid grid-flow-row grid-cols-1 border-b border-dashed sm:grid-cols-3 md:h-[150px]")}
           >
             <ModernCardContainer className={cn("flex flex-col p-6 sm:col-span-2", index % 2 === 0 && "sm:order-1")}>
               <ModernCardTitle label={sponser.label}>{sponser.name}</ModernCardTitle>
