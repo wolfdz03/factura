c49220e3df92fa7b643664aaa44f5ce9bbe69f62
Gurbinder <gurbindersingh8727@gmail.com>
2025-07-07 23:39:36 +0530

    refactor: remove cloudflare dependencies

--- packages/utilities/src/env/index.ts
+++ packages/utilities/src/env/index.ts
@@ -7,6 +7,11 @@ export const env = createEnv({
     DATABASE_URL: z.string(),
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
+    CF_R2_ENDPOINT: z.string(),
+    CF_R2_ACCESS_KEY_ID: z.string(),
+    CF_R2_SECRET_ACCESS_KEY: z.string(),
+    CF_R2_BUCKET_NAME: z.string(),
+    CF_R2_PUBLIC_DOMAIN: z.string(),
   },
   client: {
     NEXT_PUBLIC_POSTHOG_HOST: z.string(),
@@ -20,6 +25,11 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    CF_R2_ENDPOINT: process.env.CF_R2_ENDPOINT,
+    CF_R2_ACCESS_KEY_ID: process.env.CF_R2_ACCESS_KEY_ID,
+    CF_R2_SECRET_ACCESS_KEY: process.env.CF_R2_SECRET_ACCESS_KEY,
+    CF_R2_BUCKET_NAME: process.env.CF_R2_BUCKET_NAME,
+    CF_R2_PUBLIC_DOMAIN: process.env.CF_R2_PUBLIC_DOMAIN,
     // =========== PUBLIC ===========
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
