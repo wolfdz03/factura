8481e6ab886884c10627e8e3aeb1b5fda7f17ea1
amarnath666 <amarnathdhumal2001@gmail.com>
2025-09-07 09:34:39 +0530

    fix: fancybadge button width in low width mobile

--- apps/web/src/components/ui/fancy-badges.tsx
+++ apps/web/src/components/ui/fancy-badges.tsx
@@ -4,7 +4,7 @@ const FancyBadgeWithBorders = ({ children }: { children: string }) => {
   return (
     <div className="flex flex-row items-center gap-2 px-6">
       <div className="flex flex-row items-center">
-        <div className="from-muted h-px w-24 bg-gradient-to-l to-transparent sm:w-40"></div>
+        <div className="from-muted h-px w-20 bg-gradient-to-l to-transparent sm:w-40"></div>
         <div className="bg-muted/20 h-1.5 w-1.5 border"></div>
       </div>
       <div className="bg-muted/20 jetbrains-mono relative flex h-7 flex-row items-center gap-2 rounded-md border px-4 text-sm font-medium">
@@ -12,7 +12,7 @@ const FancyBadgeWithBorders = ({ children }: { children: string }) => {
       </div>
       <div className="flex flex-row items-center">
         <div className="bg-muted/20 h-1.5 w-1.5 border"></div>
-        <div className="from-muted h-px w-24 bg-gradient-to-r to-transparent sm:w-40"></div>
+        <div className="from-muted h-px w-20 bg-gradient-to-r to-transparent sm:w-40"></div>
       </div>
     </div>
   );
